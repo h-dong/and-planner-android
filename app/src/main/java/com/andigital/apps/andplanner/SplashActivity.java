fb36c44c36b212a5782a3fe258d13c4619eb4dbd
Hao Dong <dong_hao@hotmail.co.uk>
2016-07-12 14:16:26 +0100

    Added splash screen activity in the new app

---
+++ app/src/main/java/com/andigital/apps/andplanner/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.andigital.apps.andplanner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SplashActivity extends AppCompatActivity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_splash);
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    sleep(500);
+
+                    Intent i = new Intent(getBaseContext(), ScheduleActivity.class);
+                    startActivity(i);
+                    finish();
+                } catch (Exception e) {
+                    Log.e("ERROR", e.getMessage());
+                }
+            }
+        };
+        background.start();
+    }
+
+}
